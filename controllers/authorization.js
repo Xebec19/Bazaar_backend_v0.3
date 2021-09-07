8f77f519a5731d67a6d2947337222d3645422511
xebec19 <rohant946@gmail.com>
2021-09-07 08:58:53 +0530

    set up jwt in register endpoint

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -1,32 +1,43 @@
 import bcrypt from 'bcryptjs'
-import * as jsonwt from 'jsonwebtoken'
+import jsonwt from 'jsonwebtoken'
 
 import logger from '../setup/logger.js'
 import { jwtSecret } from '../utils/environment.js'
 
+/**
+ * @route /public/register
+ * @param {name,email,password,phoneNumber} req 
+ * @param {userId,token} res 
+ */
 export const register = async (req, res) => {
-    const { email, password } = req.body;
+    let { name, email, password, phoneNumber } = req.body;
     try {
         bcrypt.genSalt(10, function (err, salt) {
             bcrypt.hash(password, salt, function (err, hash) {
                 if (err) throw new Error(err);
+                if (!hash) throw new Error(err);
                 password = hash;
             });
         });
     }
     catch (error) {
-        logger.error(error);
-        res.status(401).json({ message: "--error occurred while hashing password" }).end();
+        logger.error(error.message);
+        res.status(401).json({ message: "--error occurred while hashing password", data: false }).end();
     }
     const payload = { email };
     try {
-        jsonwt.sign(payload,)
+        if (!jwtSecret) throw new Error('Secret is not defined');
+        const token = jsonwt.sign({
+            data: 'foobar'
+        }, 'secret', { expiresIn: '1h' });
+        ;
+        if (!token) throw new Error('Token not generated');
+        res.status(201).json({ message: 'user registered successfully', status: true, token: 'Bearer' + token }).end();
     }
     catch (error) {
         logger.error(error);
-        res.status(401).json({ message: "--error occurred while hashing password" }).end();
+        res.status(401).json({ message: "--error occurred while generating token", status: false, token: false }).end();
     }
-    res.status(201).json({ message: 'Well done' }).end();
 }
 
 export const logIn = async (req, res) => {
